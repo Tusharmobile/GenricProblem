f9db0fcf617e0eb7974ff864b9229e87c896a395
tushar <tusharshitre123@gmail.com>
2022-02-02 15:45:57 +0530

    add comparision of 3 genrics

---
+++ GenricProblem/GenricProblem/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenricProblem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter three numbers:");
+            int a = Convert.ToInt32(Console.ReadLine());
+            int b = Convert.ToInt32(Console.ReadLine());
+            int c = Convert.ToInt32(Console.ReadLine());
+            int max = Genric.FindMaxIntNumber(a, b, c);
+            if (max == 0)
+
+            {
+                Console.WriteLine("All three numbers are equal!!!");
+            }
+            else
+            {
+                
+                Console.WriteLine("Maximum among three:" + max);
+                Console.ReadLine();
+               
+            }
+        } 
+    }
+}
+    
+
